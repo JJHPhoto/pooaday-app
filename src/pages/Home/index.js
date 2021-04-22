564bc19e5a7c1669e25024a711338e67e3b5da41
Joshua J. Haller <phone@joshuajhaller.com>
2021-04-22 09:55:13 -0700

    Keep getting hook errors on MDBReact components.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
 import LoginForm from "../../components/LoginForm";
+import Navbar from "../../components/Navbar";
 // import Developers from "../Developers";
 
 function Home() {
   return (
     <div className="container">
+      <Navbar />
       <Header />
       <LoginForm />
       {/* <Developers /> */}
