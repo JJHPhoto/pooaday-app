ac3dc036f8e30e2f52b4bdc884213f6fbd36b1cf
Joshua J. Haller <phone@joshuajhaller.com>
2021-05-17 09:46:19 -0700

    Working on Netlify build errors.

--- src/components/TimeCard/TimeCard.jsx
+++ src/components/TimeCard/TimeCard.jsx
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import { Card, Accordion } from "react-bootstrap";
 
 const TimeCard = (props) => {
-	return (
-		<Card>
-			<Accordion.Toggle as={Card.Header} eventKey="0">
-				Time
-			</Accordion.Toggle>
-			<Accordion.Collapse eventKey="0">
-				<Card.Body>Hello! I'm the body</Card.Body>
-			</Accordion.Collapse>
-		</Card>
-	);
+  return (
+    <Card>
+      <Accordion.Toggle as={Card.Header} eventKey="0">
+        Time
+      </Accordion.Toggle>
+      <Accordion.Collapse eventKey="0">
+        <Card.Body>Hello! I'm the body</Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  );
 };
 
 export default TimeCard;
